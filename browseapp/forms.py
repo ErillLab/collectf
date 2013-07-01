1bd5e355719333613f331a91872a62ef38643653
Sefa Kilic <sefakilic@gmail.com>
2013-07-01 16:05:34 -0400

    browse form checkbox for motif-associated/ChIP-seq sequence

--- browseapp/forms.py
+++ browseapp/forms.py
@@ -10,20 +10,8 @@ class BrowseForm(forms.Form):
                                      label="species",
                                      initial=0)
 
-    '''
-    # generate techniques field
-    # get available techniques from db
-    choices = []
-    # used twitter-bootstrap tooltip for exp technique description
-    description_markup = u'<span data-toggle="popover" data-original-title="%s" data-content="%s">%s</span>'
-    for t in models.ExperimentalTechnique.objects.order_by('name'):
-        choices.append((t.technique_id,
-                        mark_safe(description_markup % (t.name, t.description, t.name))))
-    techniques = forms.MultipleChoiceField(choices = choices,
-                                           label = "Techniques",
-                                           required = False,
-                                           widget = forms.CheckboxSelectMultiple(),
-                                           initial = [c[0] for c in choices])
-    '''
-    
+    browse_motif_associated_sites = forms.BooleanField(required=False)
+    browse_not_motif_associated_sites = forms.BooleanField(required=False)
+
+
     
