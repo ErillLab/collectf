323ab9dfb5966d18513f37223fed87ac81848b2b
Sefa Kilic <sefakilic@gmail.com>
2013-10-21 20:29:56 -0400

    memcache timeout

--- collectf/settings_local.py
+++ collectf/settings_local.py
@@ -204,5 +204,6 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 2500000, # almost 30 days
     }
 }
