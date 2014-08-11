02cf4a5895e9b668fb7a6cb29ce52dc914acf20f
Sefa Kilic <sefakilic@gmail.com>
2014-08-11 14:32:06 -0400

    pylint

--- curate/urls.py
+++ curate/urls.py
@@ -1,29 +1,35 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('curate.add_publication',
+urlpatterns = patterns(
+    'curate.add_publication',
     url(r'^add_pubmed/$', 'pubmed_submission'),
     url(r'^add_non_pubmed/$', 'non_pubmed_submission'),
 )
 
-urlpatterns += patterns('curate.add_curation',
+urlpatterns += patterns(
+    'curate.add_curation',
     url(r'^curation/$', 'curation'),
     url(r'^high_throughput_curation/$', 'high_throughput_curation'),
 )
 
-urlpatterns += patterns('curate.edit_curation',
+urlpatterns += patterns(
+    'curate.edit_curation',
     url(r'^edit_curation/(?P<cid>\d+)/$', 'edit_curation'),
 )
 
-urlpatterns += patterns('curate.add_TF',
+urlpatterns += patterns(
+    'curate.add_TF',
     url(r'^add_TF/$', 'add_TF'),
     url(r'^add_TF_family/$', 'add_TF_family'),
 )
 
-urlpatterns += patterns('curate.add_technique',
+urlpatterns += patterns(
+    'curate.add_technique',
     url(r'^add_technique/$', 'add_technique'),
 )
 
-urlpatterns += patterns('curate.validate_curation',
+urlpatterns += patterns(
+    'curate.validate_curation',
     url(r'^validate_home/$', 'home'),
     url(r'^validate_edit/$', 'view_edit_validate'),
     url(r'^validate_view_curations/$', 'view_validated_curations'),
