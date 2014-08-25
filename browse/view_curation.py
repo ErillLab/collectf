bdfea249ad4a584a1d297523f9d18e46e5ac19ed
Sefa Kilic <sefakilic@gmail.com>
2014-08-25 09:07:04 -0400

    view_curation: 404 if curation doesn't exist

--- browse/view_curation.py
+++ browse/view_curation.py
@@ -1,12 +1,13 @@
 """Handler to view curation"""
 from base import models
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 
 def view_curation(request, cid):
     """Handler function for curation view"""
-    curation = models.Curation.objects.get(curation_id=cid)
+    curation = get_object_or_404(models.Curation, curation_id=cid)
     return render_to_response("view_curation.html",
                               {'curation': curation},
-                              context_instance = RequestContext(request))
+                              context_instance=RequestContext(request))
 
