b613ac7ff34d784e2224d32acedadfb793e3a1af
Sefa Kilic <sefa1@umbc.edu>
2015-09-22 22:40:46 -0400

    remove extra template tag, add login/logout urls

--- base/models.py
+++ base/models.py
@@ -500,7 +500,7 @@ class ExperimentalTechnique(models.Model):
                                        choices=FUNCTION_CATEGORIES, null=True)
     categories = models.ManyToManyField("ExperimentalTechniqueCategory")
 
-   # the foll. was added by Dinara
+    # the foll. was added by Dinara
     EO_term = models.CharField(max_length=50, null=True, blank=True)
 
     def __unicode__(self):
--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('base.views',
     url(r'^$', 'home'),
     url(r'^get_weblogo/$', 'get_weblogo'),
+    url(r'^login$', 'login'),
+    url(r'^logout$', 'logout'),
 )
