ae07635c47767e5028fced605b9b1b67dfd55bd1
Sefa Kilic <sefakilic@gmail.com>
2015-09-29 15:08:26 -0400

    Find UniProt mappings for all RefSeq accessions.
    Out of 490 RefSeq accession numbers, only 27 of them have no UniProt
    candidate. Here is the distribution of number of UniProt accessions per
    RefSeq accession.
    
    {1: 324, 2: 53, 0: 27, 4: 27, 5: 10, 7: 7, 8: 7, 3: 6, 6: 4, 18: 4, 9:
    3, 11: 2, 21: 2, 36: 2, 10: 1, 12: 1, 15: 1, 16: 1, 17: 1, 282: 1, 37:
    1, 439: 1, 59: 1, 71: 1, 78: 1, 98: 1}
    
    Next step is to resolve mappings from a RefSeq to multiple UniProt
    accessions.

--- scripts/migrate_to_uniprot.py
+++ scripts/migrate_to_uniprot.py
@@ -10,6 +10,7 @@ import os
 import pickle
 import random
 import re
+from pprint import pprint
 
 from Bio import Entrez
 from tqdm import tqdm
@@ -26,10 +27,6 @@ def fetch_ncbi_protein_record(accession):
     records = Entrez.read(handle)
     return records[0]
 
-def parse_wp_accession(rec):
-    """Extracts the non-redundant WP accession from a protein record."""
-    return rec['GBSeq_contig']
-
 def parse_ncbi_taxonomy_id(rec):
     """Extracts the NCBI taxonomy id from the given protein record."""
     # Not safe, but OK for one-time script.
@@ -41,9 +38,9 @@ def parse_ncbi_taxonomy_id(rec):
     tax_id = tax_feature['GBQualifier_value'].replace('taxon:', '')
     return tax_id
 
-def refseq_to_wp(rec):
+def parse_wp_accession(rec):
     """Returns a WP accession for a given NP/YP accession."""
-    contig = parse_wp_accession(rec)
+    contig = rec['GBSeq_contig']
     wp_acc = re.match(r'join\((WP_\d+.\d):\d..\d+\)', contig).group(1)
     return wp_acc
 
@@ -92,19 +89,19 @@ def get_all_proteins():
     if not os.path.isfile(json_dump_file):
         proteins = [protein.protein_accession
                     for protein in models.TFInstance.objects.all()]
-        json.dump(proteins, open(json_dump_file, 'w'))
+        json.dump(proteins, open(json_dump_file, 'w'), indent=4)
 
     return json.load(open(json_dump_file))
 
 def fetch_all_ncbi_protein_records():
     """For all proteins, retrieves the NCBI records."""
+    refseq_accessions = get_all_proteins()
     json_dump_file = os.path.join(DATA_DIR, 'ncbi_records.json')
     if not os.path.isfile(json_dump_file):
-        proteins = get_all_proteins()
         all_records = {}
-        for refseq in tqdm(proteins):
+        for refseq in tqdm(refseq_accessions):
             all_records[refseq] = fetch_ncbi_protein_record(refseq)
-        json.dump(all_records, open(json_dump_file, 'w'))
+        json.dump(all_records, open(json_dump_file, 'w'), indent=4)
 
     return json.load(open(json_dump_file))
 
@@ -112,19 +109,48 @@ def refseq_to_uniprot(refseq_accessions):
     """Maps the given RefSeq accession to UniProt ID."""
     return uniprot.map(refseq_accessions, f='P_REFSEQ_AC', t='ACC')
 
-def refseq_to_uniprot_batch(refseq_accs):
-    """Given a list of RefSeq accessions, returns the mapping to UniProt. """
-    all_mappings = {}
-    mapping = refseq_to_uniprot(refseq_accs)
-    for refseq_acc in tqdm(refseq_accs):
-        all_mappings[refseq_acc] = mapping.get(refseq_acc, None)
-        if (not all_mappings[refseq_acc] and
-            (refseq_acc.startswith('NP') or refseq_acc.startswith('YP'))):
-            wp_acc = refseq_to_wp(refseq_acc)
-            all_mappings[refseq_acc] = {wp_acc}
-            mapping = refseq_to_uniprot(wp_acc)
-            all_mappings[wp_acc] = mapping.get(wp_acc, None)
-    return all_mappings
+def map_refseq_to_uniprot():
+    """Given a list of RefSeq accessions, returns the mapping to UniProt."""
+
+    def map_refseq_to_uniprot_helper():
+        refseq_accs = get_all_proteins()
+        ncbi_records = fetch_all_ncbi_protein_records()
+        all_mappings = {}
+
+        uniprot_mapping = refseq_to_uniprot(refseq_accs)
+        print "Found %d mappings." % len(uniprot_mapping)
+        for refseq_acc in tqdm(refseq_accs):
+            all_mappings[refseq_acc] = list(uniprot_mapping.get(refseq_acc, {}))
+            
+        # For not mapped NP and YP accessions, get their WP accession and try
+        # mapping again.
+        not_mapped_refseqs = [refseq_acc for refseq_acc in refseq_accs
+                              if not all_mappings[refseq_acc] and
+                              (refseq_acc.startswith('NP') or
+                               refseq_acc.startswith('YP'))]
+        print ("Getting WP accs for %d NP/YP accs." % len(not_mapped_refseqs))
+
+        wp_accesssion_mapping = {refseq:parse_wp_accession(ncbi_records[refseq])
+                                 for refseq in not_mapped_refseqs}
+
+        uniprot_mapping = refseq_to_uniprot(wp_accesssion_mapping.values())
+        print "Found %d additional mappings" % len(uniprot_mapping)
+        for not_mapped_refseq in not_mapped_refseqs:
+            wp = wp_accesssion_mapping[not_mapped_refseq]
+            all_mappings[not_mapped_refseq] = list(uniprot_mapping.get(wp, {}))
+
+        print "UniProt mapping size distribution:"
+        c = Counter(map(len, all_mappings.values()))
+        pprint(c)
+
+        return all_mappings
+
+    json_dump_file = os.path.join(DATA_DIR, "refseq_to_uniprot.json")
+    if not os.path.isfile(json_dump_file) or True:
+        all_mappings = map_refseq_to_uniprot_helper()
+        json.dump(all_mappings, open(json_dump_file, 'w'), indent=4)
+
+    return json.load(open(json_dump_file))
 
 def mock_get_all_proteins():
     proteins =[u'NP_799324', u'NP_231738', u'YP_006516164',
