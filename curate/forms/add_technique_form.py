dea940a6ed71ec4884d110684836f95267e166da
Sefa Kilic <sefa1@umbc.edu>
2015-10-09 14:20:48 -0400

    Lint modules in curate/forms

--- curate/forms/add_technique_form.py
+++ curate/forms/add_technique_form.py
@@ -6,9 +6,13 @@ import base.models as models
 class AddTechniqueForm(forms.Form):
     name = forms.CharField(label="Name")
     description = forms.CharField(label="Description", widget=forms.Textarea)
-    type = forms.ChoiceField(choices = models.ExperimentalTechnique.FUNCTION_CATEGORIES)
-    categories = forms.ModelMultipleChoiceField(queryset=models.ExperimentalTechniqueCategory.objects.order_by('name'))
+    type = forms.ChoiceField(
+        choices = models.ExperimentalTechnique.FUNCTION_CATEGORIES)
+    categories = forms.ModelMultipleChoiceField(
+        queryset=models.ExperimentalTechniqueCategory.objects.order_by('name'))
 
  # the foll. was added by Dinara
-    EO_term = forms.CharField(label="ECO term", widget=forms.TextInput(attrs={'class':"bp_form_complete-ECO-uri", 
-    																			'name':"a",'size':"100"}))
+    EO_term = forms.CharField(
+        label="ECO term",
+        widget=forms.TextInput(attrs={
+            'class':"bp_form_complete-ECO-uri", 'name':"a", 'size':"100"}))
