8215677aea108ef60658cd64c280b3d07d188973
Sefa Kilic <sefa1@umbc.edu>
2015-10-12 01:39:32 -0400

    Remove print statements.

--- browse/static_reports.py
+++ browse/static_reports.py
@@ -7,10 +7,8 @@ from collectf import settings
 
 def get_static_reports(filename):
     """Gets the reports and ensemble report."""
-    print 'getting static reports..'
     reports = pickle.load(open(os.path.join(
         settings.PICKLE_ROOT, 'reports', filename + '.pkl')))
     ensemble_report = pickle.load(open(os.path.join(
         settings.PICKLE_ROOT, 'ensemble_reports', filename + '.pkl')))
-    print 'done.'
     return reports, ensemble_report
