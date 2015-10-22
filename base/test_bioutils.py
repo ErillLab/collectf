65ef40b6d71ce1ff9329fc27133bd1250ed42499
Sefa Kilic <sefa1@umbc.edu>
2015-10-21 23:12:13 -0400

    Add genome record fetch tests

--- base/test_bioutils.py
+++ base/test_bioutils.py
@@ -18,7 +18,15 @@ class EntrezTest(TestCase):
             record['Title'],
             'Formate assay in body fluids: application in methanol poisoning.')
 
-    def test_none_on_invalid_pmid(self):
+    def test_returns_nothing_on_invalid_pmid(self):
         record = bioutils.get_pubmed(-1)
         self.assertIsNone(record)
     
+    def test_fetches_genome_record(self):
+        record = bioutils.get_genome('NC_000913.2')
+        self.assertEqual(record.id, 'NC_000913.2')
+        self.assertEqual(str(record.seq[:5]), 'AGCTT')
+
+    def test_returns_nothing_on_invalid_genome_accession(self):
+        record = bioutils.get_genome('NC_invalid')
+        self.assertIsNone(record)
