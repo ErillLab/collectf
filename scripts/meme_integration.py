ed2180a253ddff6da321be8298619484829320a3
Sefa Kilic <sefakilic@gmail.com>
2015-10-29 14:49:20 -0400

    Update export for MEME integration

--- scripts/meme_integration.py
+++ scripts/meme_integration.py
@@ -1,31 +1,35 @@
 """This module generates the file containing motifs in CollecTF."""
 
 import os
-from tqdm import tqdm
 
 from base import models
 from base.bioutils import build_motif
-from browse.static_reports import get_static_reports
-from browse.dbxref import to_uniprot_dbxref
+from browse import dbxref
+from browse import motif_report
 from collectf import settings
 
 def generate_motif_file():
     """Generates motif file containing all motifs for each TF-instance."""
-    export_file = os.path.join(settings.STATICFILES_DIRS[0], 'collectf_meme.cm')
+    export_file = os.path.join(settings.STATIC_ROOT, 'collectf_meme.cm')
     with open(export_file, 'w') as f:
-        for TF_instance in tqdm(models.TFInstance.objects.all()):
-            reports, _ = get_static_reports(
-                'TF_instance_%s' % TF_instance.TF_instance_id)
+        for TF_instance in models.TFInstance.objects.all():
+            print TF_instance
+            curation_site_instances = models.Curation_SiteInstance.objects.filter(
+                curation__TF_instances=TF_instance)
+            reports = motif_report.make_reports(curation_site_instances)
             if reports:
-                motif_sites = reports[0].get_single_motif()
-                if len(motif_sites) < 5:
+                sites = reports[0].align_sites()
+                if len(sites) < 10:
                     continue
-                motif = build_motif(motif_sites)
-                f.write('>%s\n' % to_uniprot_dbxref(TF_instance.TF_instance_id))
-                f.write('A| %s\n' % ' '.join(map(str, motif.counts['A'])))
-                f.write('C| %s\n' % ' '.join(map(str, motif.counts['C'])))
-                f.write('G| %s\n' % ' '.join(map(str, motif.counts['G'])))
-                f.write('T| %s\n' % ' '.join(map(str, motif.counts['T'])))
+                motif = build_motif(sites)
+                f.write('>%s %s_%s\n' % (
+                    dbxref.to_uniprot_dbxref(TF_instance.TF_instance_id),
+                    reports[0].TF_name,
+                    reports[0].species_name.replace(' ', '_')))
+                f.write('%s\n' % '\t'.join(map(str, motif.counts['A'])))
+                f.write('%s\n' % '\t'.join(map(str, motif.counts['C'])))
+                f.write('%s\n' % '\t'.join(map(str, motif.counts['G'])))
+                f.write('%s\n' % '\t'.join(map(str, motif.counts['T'])))
 
 def run():
     """Entry point for the script."""
