ada7bd69c960c7a9a311f6226d65a45e3dce992c
Sefa Kilic <sefa1@umbc.edu>
2015-11-02 22:19:11 -0500

    Travis settings file

--- src/collectf/settings/base.py
+++ src/collectf/settings/base.py
@@ -71,22 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'collectf.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'collectfdb_devel',
-        'USER': 'sefa',
-        'PASSWORD': '46544654',
-        'HOST': 'localhost',
-        'PORT': ''
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
