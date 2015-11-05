8fa37fe3cbe06b6c027ee79850034f2c1d632fe7
Sefa Kilic <sefakilic@gmail.com>
2015-11-05 12:41:04 -0500

    base.html stuff

--- src/browse/urls.py
+++ src/browse/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .views import test_views
+from . import test_view
+from . import homepage_view
 
 urlpatterns = [
-    url(r'^test_view$', test_views.test_view),
+    url(r'^test_view$', test_view.test_view),
+    url(r'^home$', homepage_view.homepage),
 ]
