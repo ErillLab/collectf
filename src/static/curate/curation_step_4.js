f43d245e01d517245ecbad033b9f75658c6aee5d
Sefa Kilic <sefakilic@gmail.com>
2015-11-19 16:13:20 -0500

    move js to separate files

---
+++ src/static/curate/curation_step_4.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function() {
+    // Adds sequence class to all fields (site matches), to make them fixed-width.
+    $('.form-group').find('label:first').addClass('sequence');
+});
---
+++ src/static/curate/curation_step_7.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // Change the ul style (hide bullet points on the list of gene regulations)
+    $('ul').css('list-style-type', 'none');
+});
