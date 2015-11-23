2420292656326e17ce8c68f04c7ca4484366791f
Sefa Kilic <sefakilic@gmail.com>
2015-11-23 14:27:56 -0500

    Add database settings

--- src/collectf/settings/production.py
+++ src/collectf/settings/production.py
@@ -1,1 +1,12 @@
 from .base import *             # NOQA
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'collectfdb',
+        'USER': 'erilllab',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
