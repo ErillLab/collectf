c0a2d2176b06de12f287a0eea51e9fd104b4b984
Sefa Kilic <sefakilic@gmail.com>
2015-12-22 12:50:49 -0500

    Add script to dump alignments

--- src/scripts/db_dump.py
+++ src/scripts/db_dump.py
@@ -1,6 +1,7 @@
 """The module for generating CollecTF release file."""
 
 import os
+from zipfile import ZipFile
 
 from django.conf import settings
 
@@ -13,7 +14,7 @@ def db_dump():
     """Generates the release file containing all binding sites in CollecTF."""
     curation_site_instances = models.Curation_SiteInstance.objects.all()
     reports = motif_report.build_motif_reports(curation_site_instances)
-    meta_sites = [meta_site 
+    meta_sites = [meta_site
                   for report in reports
                   for meta_site in report.meta_sites]
     tsv_raw = export.export_tsv(meta_sites)
@@ -21,6 +22,23 @@ def db_dump():
         settings.STATICFILES_DIRS[0], 'collectf_export.tsv')
     with open(export_file, 'w') as f:
         f.write(tsv_raw)
+
+
+def all_alignments():
+    """Generates aligned binding moitfs."""
+    curation_site_instances = models.Curation_SiteInstance.objects.all()
+    reports = motif_report.build_motif_reports(curation_site_instances)
+    zip_file_name = os.path.join(settings.STATICFILES_DIRS[0],
+                                 'collectf_alignments.zip')
+    with ZipFile(zip_file_name, 'w') as zip:
+        for report in reports:
+            if len(report.aligned_sites) >= 8:
+                tf = report.TF_name
+                species = report.short_species_name.replace('.', '_')
+                print tf, species
+                filename = '%s_%s.txt' % (tf, species)
+                zip.writestr(filename, '\n'.join(report.aligned_sites))
+
 
 def run():
     """Entry point for the script."""
