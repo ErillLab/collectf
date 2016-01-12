c594e53de2bfec2a8acfd56f437bd6391915e44f
Sefa Kilic <sefakilic@gmail.com>
2016-01-12 12:01:27 -0500

    Fix #70

--- src/core/templatetags/utiltags.py
+++ src/core/templatetags/utiltags.py
@@ -5,6 +5,9 @@ See Django documentation on template tags:
 https://docs.djangoproject.com/en/dev/ref/templates/builtins/
 """
 
+import re
+
+from django.utils.safestring import mark_safe
 from django.template import Library
 register = Library()
 
@@ -25,3 +28,19 @@ def get(o, index):
 def get_keys(dictionary):
     """Return all keys of a dictionary."""
     return sorted(dictionary.keys())
+
+
+@register.filter
+def HTMLify(description_text):
+    """Turns the description text into HTML."""
+    # replace PMIDs
+    description_text = re.sub(
+        '\[PMID::([0-9]+)\]',
+        '<sup><a href="http://www.ncbi.nlm.nih.gov/pubmed/?term=\g<1>">[\g<1>]</a></sup>',
+        description_text)
+    # replace PFAMs
+    description_text = re.sub(
+        '\[PFAM::(\w+)\]',
+        '<sup><a href="http://pfam.sanger.ac.uk/family/\g<1>">[\g<1>]</a></sup>',
+        description_text)
+    return mark_safe(description_text)
