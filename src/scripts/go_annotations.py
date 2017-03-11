dc17715150aa6dc4386fccb5d6d1a4ce8e2b5f06
ivanerill <erill@umbc.edu>
2017-03-10 23:52:30 -0500

    wrapping up

--- src/scripts/go_annotations.py
+++ src/scripts/go_annotations.py
@@ -55,6 +55,12 @@ and for activation/repression, we get:
 
 Again, this means two annotations if the TF activates at least one gene and
 represses at least one gene.
+
+03/09/2017
+- Modified script so that it adds WITH field for IPI-derived codes, using the
+  genome accession from RefSeq as ID for WITH field
+- Modified script so that it also generates a cellular component annotation
+  using GO:0032993 - protein-DNA complex for binding-supported experiments.
 """
 
 import os
@@ -88,7 +94,7 @@ def new_TF_centric_gpad_entry(uniprot_id, go_id, db_ref,
                 annotation_properties='')
 
 
-def new_gene_centric_gpad_entry(uniprot_id, go_id, db_ref, evidence_code):
+def new_gene_centric_gpad_entry(uniprot_id, go_id, db_ref, evidence_code, with_field):
     """Returns a dictionary for a gene-centric GO annotation.
 
     See http://geneontology.org/page/gene-product-association-data-gpad-format
@@ -99,7 +105,7 @@ def new_gene_centric_gpad_entry(uniprot_id, go_id, db_ref, evidence_code):
                 go_id=go_id,
                 db_ref=('PMID:' + db_ref),
                 evidence_code=evidence_code,
-                with_from='',
+                with_from=with_field,
                 interacting_taxon_id='',
                 date=time.strftime('%Y%m%d'),
                 assigned_by='CollecTF',
@@ -141,10 +147,9 @@ def TF_centric_binding_annotations(TF_instance):
     for reg in (r for r in exp_regulations if r.mode in ['ACT', 'REP']):
         pmid = reg.ref_pmid
         for tech in reg.binding_experimental_techniques:
-            with_field=''
+            with_field='' #define WITH field, fill it up only if IPI evidence used
             if tech.EO_term in get_ECO_IPI_terms():
                 with_field='RefSeq:'+reg.curation_site_instance.site_instance.genome.genome_accession
-#            print reg.curation_site_instance.site_instance.genome.genome_accession
             if reg.mode == 'ACT':
                 go_annotations.append(new_TF_centric_gpad_entry(
                     uniprot_id, 'GO:0001216', pmid, tech.EO_term, 'enables',with_field))
@@ -153,6 +158,8 @@ def TF_centric_binding_annotations(TF_instance):
                     uniprot_id, 'GO:0001217', pmid, tech.EO_term, 'enables',with_field))
             go_annotations.append(new_TF_centric_gpad_entry(
                 uniprot_id, 'GO:0000976', pmid, tech.EO_term, 'enables',with_field))
+            go_annotations.append(new_TF_centric_gpad_entry(
+                uniprot_id, 'GO:0032993', pmid, tech.EO_term, 'part_of',with_field))
         annotated_pmids.add(pmid)
 
     # If there is evidence of TF-mediated regulation for at least one gene,
@@ -162,13 +169,15 @@ def TF_centric_binding_annotations(TF_instance):
         pmid = reg.ref_pmid
         if pmid not in annotated_pmids:
             for tech in reg.binding_experimental_techniques:
-                with_field=''
+                with_field='' #define WITH field, fill it up only if IPI evidence used
                 if tech.EO_term in get_ECO_IPI_terms():
                     with_field='RefSeq:'+reg.curation_site_instance.site_instance.genome.genome_accession            
                 go_annotations.append(new_TF_centric_gpad_entry(
                     uniprot_id, 'GO:0001130', pmid, tech.EO_term, 'enables',with_field))
                 go_annotations.append(new_TF_centric_gpad_entry(
                     uniprot_id, 'GO:0000976', pmid, tech.EO_term, 'enables',with_field))
+                go_annotations.append(new_TF_centric_gpad_entry(
+                    uniprot_id, 'GO:0032993', pmid, tech.EO_term, 'part_of',with_field))
                 annotated_pmids.add(pmid)
 
     # If there is no regulation evidence, simply report binding
@@ -176,11 +185,13 @@ def TF_centric_binding_annotations(TF_instance):
         pmid = reg.ref_pmid
         if pmid not in annotated_pmids:
             for tech in reg.binding_experimental_techniques:
-                with_field=''
+                with_field='' #define WITH field, fill it up only if IPI evidence used
                 if tech.EO_term in get_ECO_IPI_terms():
                     with_field='RefSeq:'+reg.curation_site_instance.site_instance.genome.genome_accession                
                 go_annotations.append(new_TF_centric_gpad_entry(
                     uniprot_id, 'GO:0043565', pmid, tech.EO_term, 'enables',with_field))
+                go_annotations.append(new_TF_centric_gpad_entry(
+                    uniprot_id, 'GO:0032993', pmid, tech.EO_term, 'part_of',with_field))
                 annotated_pmids.add(pmid)
 
     return set(map(gpad_entry_to_str, go_annotations))
@@ -192,7 +203,7 @@ def TF_centric_regulation_annotations(TF_instance):
     go_annotations = []
     for reg in get_exp_regulations(TF_instance):
         for tech in reg.expression_experimental_techniques:
-            with_field=''
+            with_field='' #define WITH field, fill it up only if IPI evidence used
             if tech.EO_term in get_ECO_IPI_terms():
                 with_field='RefSeq:'+reg.curation_site_instance.site_instance.genome.genome_accession            
             if reg.mode == 'ACT':
@@ -221,12 +232,35 @@ def batch_refseq_accession(genome_accession):
     gene_dict = {}
     for gene in genes:
         if gene['protein_id'] and len(gene['protein_id']) == 1:
-            protein_id = gene['protein_id'][0].split('.')[0]
+            protein_id = gene['protein_id'][0]#.split('.')[0]
             gene_dict[gene['locus_tag']] = protein_id
             for old_locus_tag in gene['old_locus_tag']:
                 gene_dict[old_locus_tag] = protein_id
     return gene_dict
 
+def retrieve_genome(genome_accession):
+    """Retrieves the genome, returns it as a list of genes"""
+    print "Downloading", genome_accession
+    genome_rec = entrez_utils.get_genome(genome_accession)
+    genes = entrez_utils.get_genes(genome_rec)
+    return genes
+    
+def get_refseq_acc(locus_tag,gene_list):
+    """Gets a locus tag and a genome gene-list, and returns a dictionary
+     {locus_tag: RefSeq_acc}, where RefSeq_acc is the product NP/YP/WP 
+     accession numbers for the protein product associated with that locus tag"""
+    gene_dict = {}
+    print locus_tag
+    print "-->"
+    for gene in gene_list:
+        if (gene['locus_tag']==locus_tag) | (gene['old_locus_tag']==locus_tag):
+            print gene['locus_tag']
+            if gene['protein_id'] and len(gene['protein_id']) == 1:
+                protein_id = gene['protein_id'][0].split('.')[0]
+                gene_dict[gene['locus_tag']] = protein_id
+                for old_locus_tag in gene['old_locus_tag']:
+                    gene_dict[old_locus_tag] = protein_id
+    return gene_dict     
 
 def refseq_to_uniprot(refseq_accession, genome_accession):
     """Maps the given RefSeq accession to a UniProt accession."""
@@ -236,35 +270,94 @@ def refseq_to_uniprot(refseq_accession, genome_accession):
     for uniprot_acc in mapping.get(refseq_accession, []):
         rec = uniprot.retrieve(uniprot_acc)
         if genome_accession in rec:
+            print "Found UNIPROT acc!"
+            print uniprot_acc
             return uniprot_acc
     return None
 
+def uniprot_to_refseq(genome_acc):
+    """Gets a genome accession. Queries uniprot to get all UniProt accessions
+       associated with genome. Downloads genome from NCBI and gets all refseq 
+       protein records associated with it.
+       For each uniprot record, checks whether it maps to a refseq record.
+       If so, it returns a dictionary with the complete mapping.
+    """
+    
+    #make uniprot query to get all uniprot IDs for a given NCBI genome accession
+    #the result is something like: [u'A0A0H3IFM0', u'B8H3X3', u'A0A0H3IXV8']
+    uniprot_accs = list(uniprot.map(genome_acc, f='REFSEQ_NT_ID' , t='ACC').get(genome_acc,[]))
+    
+    #download genome and get refseq protein accessions
+    #the result is something like: {'CCNA_00250': 'YP_002515625', 'CCNA_02456': 'YP_002517829'}
+    refseq_accs=batch_refseq_accession(genome_acc)
+    
+    gene_prot_dict = {}
+    #get mapping from all uniprot records (associated to genome) to refseq ids
+    #the result is something like:
+    #{u'A0A0H3IFM0': {u'WP_024265740.1', u'YP_008877611.1'},
+    # u'A0A0H3IXV8': {u'WP_024265959.1', u'YP_009020565.1'}}
+    if len(uniprot_accs)>0:
+        uni_refseqs=uniprot.map(uniprot_accs, f='ACC', t='P_REFSEQ_AC')
+            
+        #for each gene with mapped refseq protein record
+        for locus_tag, refseq_acc in refseq_accs.iteritems():
+            #check whether the refseq protein record has also been mapped from uniprot
+            #for each uniprot record mapped to a refseq record
+            for uni, ref in uni_refseqs.iteritems():
+                if refseq_acc in ref:
+                    protein_ids={'refseq':refseq_acc, 'uniprot':uni}
+                    #return a dictionary with locus tag as index, and protein IDs as values
+                    gene_prot_dict[locus_tag]=protein_ids
+    else:
+        print ('No UniProt accessions in:', genome_acc)
+        
+    return gene_prot_dict
 
 def gene_centric_annotations(TF_instance):
     if not TF_instance.GO_term:
         return []
-    print TF_instance
     go_annotations = []
     exp_regulations = get_exp_regulations(TF_instance)
     regulated_genomes = set(reg.gene.genome for reg in exp_regulations
                             if reg.expression_experimental_techniques)
+    
     for genome in regulated_genomes:
-        regulations_in_genome = exp_regulations.filter(gene__genome=genome)
-        refseq_accs = batch_refseq_accession(genome.genome_accession)
-        for reg in regulations_in_genome:
-            locus_tag = reg.gene.locus_tag
-            # print locus_tag
-            for tech in reg.expression_experimental_techniques:
-                refseq_acc = refseq_accs.get(locus_tag)
-                if refseq_acc:
-                    uniprot_id = refseq_to_uniprot(
-                        refseq_acc, genome.genome_accession)
-                    print refseq_acc, genome.genome_accession, uniprot_id
-                    if uniprot_id:
+
+        #get the mapping between genes in genome and uniprot accessions
+        #this looks like:
+        #{'CCNA_03751': {'refseq': 'YP_002519124.2', 'uniprot': u'A0A0H3CFU0'},
+        #'CCNA_03752': {'refseq': 'YP_002519125.3', 'uniprot': u'A0A0H3CCD6'}}
+        uni_refseq_map = uniprot_to_refseq(genome.genome_accession)
+        
+        if len(uni_refseq_map)>0:
+            #get experimental regulations in genome [i.e. regulated genes]
+            regulations_in_genome = exp_regulations.filter(gene__genome=genome)
+    
+            #for each experimentally-validated regulated gene instance
+            for reg in regulations_in_genome:
+                locus_tag = reg.gene.locus_tag   
+                
+                #if locus_tag has associated uniprot
+                if locus_tag in uni_refseq_map:
+                    #generate annotations
+                    #for each evidence source for that regulation
+                    for tech in reg.expression_experimental_techniques:
+                        with_field='' #define WITH field, fill it up only if IPI evidence used
+                        if tech.EO_term in get_ECO_IPI_terms():
+                            with_field='RefSeq:'+reg.curation_site_instance.site_instance.genome.genome_accession
+                        
+                        #get uniprot accession
+                        uniprot_id=uni_refseq_map[locus_tag]['uniprot']
+    
+                        print("Gene centric annotation:",uniprot_id)
+                        
+                        #make annotation
                         go_annotations.append(new_gene_centric_gpad_entry(
                             uniprot_id, TF_instance.GO_term.GO_term_id,
-                            reg.ref_pmid, tech.EO_term))
+                            reg.ref_pmid, tech.EO_term, with_field))            
+            
     return set(map(gpad_entry_to_str, go_annotations))
+
 
 
 def gpad_entry_to_str(gpad_entry):
@@ -293,7 +386,7 @@ def generate_gpad_file():
     for TF_instance in models.TFInstance.objects.all():
         content.extend(TF_centric_binding_annotations(TF_instance))
         content.extend(TF_centric_regulation_annotations(TF_instance))
-        #content.extend(gene_centric_annotations(TF_instance))
+        content.extend(gene_centric_annotations(TF_instance))
     export_file = os.path.join(settings.STATICFILES_DIRS[0], 'collectf.gpad')
     with open(export_file, 'w') as f:
         f.write('\n'.join(header + content))
